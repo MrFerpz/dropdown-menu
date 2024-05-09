c539f879a8f151ada6c35b59cdd4129265f69034
Fred <leferpz@gmail.com>
2024-05-09 16:29:46 +0100

    Set-up functional dropdown menu with alternating row colours

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+let menuItems = document.querySelector(".menu-items")
+let toggleBtn = document.querySelector(".open-toggle")
+
+toggleBtn.addEventListener("click", () => {
+    if (menuItems.style.display === "none") {
+        return menuItems.style.display = "block"
+    } else {
+        return menuItems.style.display = "none"
+    }
+})
